f8a4d70305b5f58cefa90e2672482deafb28fb9c
deepredinktechdepartment <109503655+deepredinktechdepartment@users.noreply.github.com>
2024-08-06 15:21:18 +0530

    Update datatables.js

--- assets/js/datatables.js
+++ assets/js/datatables.js
@@ -1,7 +1,7 @@
 // public/js/datatables.js
 
 $(document).ready(function() {
-    $('#jquery-data-table"').DataTable({
+    $('#jquery-data-table').DataTable({
         "pageLength": 50, // Default to show 50 records
         "lengthMenu": [10, 25, 50, 100], // Options for number of records per page
         "responsive": true
